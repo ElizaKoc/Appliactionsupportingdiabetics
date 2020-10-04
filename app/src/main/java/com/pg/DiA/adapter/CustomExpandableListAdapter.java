40b0a2237567696525f785b7a657f24d2259f1c9
elikoc <elikocic@gmail.com>
2020-10-04 23:10:58 +0200

    package name change

---
+++ app/src/main/java/com/pg/DiA/adapter/CustomExpandableListAdapter.java
@@ -0,0 +1,6 @@
+package com.pg.DiA.adapter;
+
+import android.widget.BaseExpandableListAdapter;
+
+public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
+}
---
+++ app/src/main/java/com/pg/DiA/ui/medicines/MedicinesViewModel.java
@@ -0,0 +1,19 @@
+package com.pg.DiA.ui.medicines;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MedicinesViewModel extends ViewModel{
+
+    private MutableLiveData<String> mText;
+
+    public MedicinesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is medicines fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
